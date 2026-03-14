0864affa9588db54529e485a625227765648d240
Jay Lao <cjaylao447@gmail.com>
2026-03-15 07:15:41 +0800

    🔧 update: fix redirect path in index component

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,5 @@
 import { Redirect } from 'expo-router';
 
 export default function Index() {
-	
-    
-    return <Redirect href="../Authentication/login" />;
-
-
+    return <Redirect href="/Authentication/login" />;
 }
