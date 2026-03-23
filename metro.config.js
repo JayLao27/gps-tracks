3788534e6611e6b4f32c8f7147917ac18e481ea1
Jay Lao <cjaylao447@gmail.com>
2026-03-23 13:09:25 +0800

    ⚙️ setup: enable package exports for ESM module resolution

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,7 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+// Supabase uses package "exports" to resolve ESM modules correctly
+config.resolver.unstable_enablePackageExports = true;
+
 module.exports = withNativeWind(config, { input: './global.css' });
